855b0110703041787a3e172c3878c6d10ae6a094
AClaich <claich.alexis@gmail.com>
2021-03-11 23:22:16 +0100

    Saving Photos to the Filesystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -26,11 +26,42 @@ export class PhotoService {
       quality: 100
     });
 
-    this.photos.unshift({
-      filePath: 'soon...',
-      webviewPath: capturedPhoto.webPath
-    });
+    const savedImageFile = await this.savePicture(capturedPhoto);
+
+    this.photos.unshift(savedImageFile);
   }
+
+  private async savePicture(cameraPhoto: CameraPhoto) {
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+
+    return {
+      filePath: fileName,
+      webviewPath: cameraPhoto.webPath
+    }
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    }
+    reader.readAsDataURL(blob);
+  })
 }
 
 export interface Photo {
